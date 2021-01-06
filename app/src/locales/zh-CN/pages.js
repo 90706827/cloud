4b5088bfc5b0ae129eb1a7d799cf60f5b8336ab6
90706827 <90706827@163.com>
2021-01-06 23:13:38 +0800

    axios

--- app/src/locales/zh-CN/pages.js
+++ app/src/locales/zh-CN/pages.js
@@ -1,6 +1,6 @@
 export default {
   'pages.layouts.userLayout.title': '中关村至臻环保股份有限公司',
-  'pages.login.accountLogin.tab': '账户密码登录',
+  'pages.login.accountLogin.tab': '账户登录',
   'pages.login.accountLogin.errorMessage': '错误的用户名和密码（admin/ant.design)',
   'pages.login.username.placeholder': '用户名: admin or user',
   'pages.login.username.required': '用户名是必填项！',
